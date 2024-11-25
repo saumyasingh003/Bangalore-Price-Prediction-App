3f70d49e1409666e82a4e735c6b9085bd2c5ed47
saumyasingh003 <saumyasingh98982@gmail.com>
2024-11-25 19:45:41 +0530

    commit

--- Server/server.py
+++ Server/server.py
@@ -2,10 +2,7 @@ from flask import Flask, request, jsonify
 import util
 import os
 from dotenv import load_dotenv
-from flask_cors import CORS
-
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
 
 @app.route('/get_location_names', methods=['GET'])
 def get_location_names():
@@ -13,31 +10,24 @@ def get_location_names():
         'locations': util.get_location_names()
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
+
     return response
 
 @app.route('/predict_home_price', methods=['GET', 'POST'])
 def predict_home_price():
-    if request.method == 'POST':
-        data = request.get_json()  # Expecting JSON data
-        total_sqft = float(data['total_sqft'])
-        location = data['location']
-        bhk = int(data['bhk'])
-        bath = int(data['bath'])
-    else:
-        # If it's a GET request, use query parameters
-        total_sqft = float(request.args.get('total_sqft'))
-        location = request.args.get('location')
-        bhk = int(request.args.get('bhk'))
-        bath = int(request.args.get('bath'))
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    bhk = int(request.form['bhk'])
+    bath = int(request.form['bath'])
 
     response = jsonify({
-        'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath)
+        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
+
     return response
 
 if __name__ == "__main__":
-    load_dotenv()  # Ensure to load the .env file
     port = int(os.getenv('FLASK_PORT', 5000))
     print(f"Starting Python Flask Server For Home Price Prediction on port {port}...")
     util.load_saved_artifacts()
